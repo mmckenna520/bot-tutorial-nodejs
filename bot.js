f6f72ea66520dc337efd3d331387c8b8a66f95db
mmckenna520 <63477500+mmckenna520@users.noreply.github.com>
2022-03-21 21:18:31 -0400

    Update bot.js
    Request a user ID

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/facetest$/;
   
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && botRegex.test(request.text) && request.user_id(39318628)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
