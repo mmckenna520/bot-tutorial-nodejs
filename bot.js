e7440376a56ff4b91acc23cd91763ee368e7e35f
mmckenna520 <63477500+mmckenna520@users.noreply.github.com>
2022-03-21 20:12:10 -0400

    Update bot.js
    check1

--- bot.js
+++ bot.js
@@ -6,8 +6,18 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/facetest$/;
-
+/*
   if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+  */
+   if(request.text) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
